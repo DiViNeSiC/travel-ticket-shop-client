29a58c0fec038a26ef4fbe615e73703b659ee0b6
DiViNeSiC <divine.beatbox@gmail.com>
2020-09-09 21:47:52 +0430

    Finished View Product Page Styling

--- src/layouts/viewProducts/productsLayout/renderOneProduct.js
+++ src/layouts/viewProducts/productsLayout/renderOneProduct.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Continent from './continent'
 import ImageSlider from './imageSlider' 
+import { Button, Label } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 export default ({
     title,
@@ -11,12 +13,52 @@ export default ({
     sold,
     imagePaths,
     createdAt,
-    creatorUser
+    creatorUser,
+    isAuth,
+    onAddCart,
+    quantity,
+    onQuantityChange,
+    error,
+    cartSuccess
 }) => {
     return (
-        <div>     
-            <ImageSlider images={imagePaths} />
-            <div>
+        <div className="product-details-container">   
+            <div className="details-image-container">
+                <ImageSlider images={imagePaths} />
+                {isAuth && 
+                    <div className="add-cart-container">
+                        <div className="quantity">
+                            <label>Quantity: </label>
+                            <input
+                                min={1}
+                                max={10}
+                                value={quantity}
+                                onChange={onQuantityChange}
+                                type="number"
+                            />
+                        </div>
+                        <Button
+                            className="btn"
+                            content="Add To Cart"
+                            icon={{ className: "plus cart" }}
+                            color={error ? 'red' : cartSuccess ? 'green' : 'vk'}
+                            onClick={onAddCart}
+                            disabled={error || cartSuccess}
+                        />
+                    </div>
+                }
+                {!isAuth && 
+                    <div className="login-add-container">
+                        <Button 
+                            as={Link} 
+                            to="/login" 
+                            content="You Must Login For Adding To Your Cart"
+                            icon={{ className: "sign in" }}
+                        />
+                    </div>
+                }
+            </div>
+            <div className="details">
                 <h2>
                     {title}
                 </h2>
@@ -26,20 +68,24 @@ export default ({
                 <h5>
                     <Continent continent={continent} />
                 </h5>
-                <div>
+                <div className="leader-details">
                     Leader: {creatorUser?.username}
                     <br/>
                     Created At: {createdAt?.split('T')[0]}
                 </div>
-                <div>
+                <div className="description-details">
                     <p>
                         {description}
                     </p>
                 </div>
                 <div>
-                    {views} View
+                    <Label className="view-message">
+                        {views} Views
+                    </Label>
                     <br/>
-                    {sold} Ticket Sold
+                    <Label className="view-message">
+                        {sold} Ticket Sold
+                    </Label>
                 </div>
             </div>
         </div>
--- src/layouts/viewProducts/viewOne.js
+++ src/layouts/viewProducts/viewOne.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Segment from '../segment/segment'
 import Dimmer from '../segment/dimmer'
 import RenderOneProduct from './productsLayout/renderOneProduct'
-import { Button } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
 
 export default ({ 
     onAddCart,
@@ -19,49 +17,29 @@ export default ({
     quantity
 }) => {
     return (
-        <div style={{ width: '400px'}}>
+        <div className="view-container">
             <div>
                 <Dimmer
                     error={error}
                     loading={loading}
-                />
+                    />
                 <Segment 
                     message={error ? error : cartSuccess}
                     type={type}
                     show={segmentShow}
                     dispatch={dispatch}
-                />
+                    />
             </div>
             {product != null && 
-                <RenderOneProduct {...product} />
-            }
-            {isAuth && 
-                <div>
-                    <input
-                        min={1}
-                        max={10}
-                        value={quantity}
-                        onChange={onQuantityChange}
-                        type="number"
-                    />
-                    <Button
-                        content="Add To Cart"
-                        icon={{ className: "plus cart" }}
-                        color={error ? 'red' : cartSuccess ? 'green' : 'vk'}
-                        onClick={onAddCart}
-                        disabled={error || cartSuccess}
-                    />
-                </div>
-            }
-            {!isAuth && 
-                <div>
-                    <Button 
-                        as={Link} 
-                        to="/login" 
-                        content="You Must Login For Adding To Your Cart"
-                        icon={{ className: "sign in" }}
-                    />
-                </div>
+                <RenderOneProduct 
+                    isAuth={isAuth} 
+                    onQuantityChange={onQuantityChange}
+                    quantity={quantity}
+                    error={error}
+                    cartSuccess={cartSuccess}
+                    onAddCart={onAddCart}
+                    {...product} 
+                />
             }
         </div>
     )
