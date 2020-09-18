1e063a6f96ccdf1aaec3f881f959b68a75a3dd1c
DiViNeSiC <divine.beatbox@gmail.com>
2020-09-18 12:00:20 +0430

    Fixed Bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import { Switch, HashRouter as Router, Route } from 'react-router-dom'
 import { GlobalProvider } from './context/contextProvider/provider'
 import routes from './routes/routes'
 // import RenderRoute from './routes/renderRoutes'
